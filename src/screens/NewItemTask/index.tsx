6bfb42f117560c7ffe6cf8003b753ddf7ffaa783
devjpedro <joaopedross.ti9@gmail.com>
2024-06-04 20:51:20 -0300

    newItemTask(optimize): adicionando react hook form no formulário de adicionar/editar tarefa

--- src/screens/NewItemTask/index.tsx
+++ src/screens/NewItemTask/index.tsx
@@ -1,11 +1,14 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { format, subDays } from "date-fns";
 import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { Alert, ScrollView, View } from "react-native";
 import { Toast } from "react-native-toast-notifications";
+import { z } from "zod";
 import { Container } from "../../components/Container";
 import { DefaultContainer } from "../../components/DefaultContainer";
 import { useUserAuth } from "../../hooks/useUserAuth";
-import { database } from '../../services';
+import { database } from "../../services";
 import { Button, ButtonClose, Content, Input, Title } from "./styles";
 
 type Props = {
@@ -15,135 +18,173 @@ type Props = {
   showButtonSave?: boolean;
   showButtonRemove?: boolean;
   selectedItemId?: string;
-}
+};
 
-export function NewItemTask({ closeBottomSheet, onCloseModal, showButtonEdit, showButtonSave, showButtonRemove, selectedItemId }: Props) {
-  const [name, setName] = useState('')
+const formSchema = z.object({
+  name: z.string().min(1, "Nome da Tarefa é obrigatória"),
+});
+
+type FormSchemaType = z.infer<typeof formSchema>;
+
+export function NewItemTask({
+  closeBottomSheet,
+  onCloseModal,
+  showButtonEdit,
+  showButtonSave,
+  showButtonRemove,
+  selectedItemId,
+}: Props) {
+  // State
   const user = useUserAuth();
   const uid = user?.uid;
   const [isEditing, setIsEditing] = useState(false);
 
-  const now = new Date()
-  const formattedDate = format(subDays(now, 1), "dd/MM/yyyy")
+  const now = new Date();
+  const formattedDate = format(subDays(now, 1), "dd/MM/yyyy");
 
-  const handleSaveItem = () => {
-    if (name === '') {
-      Alert.alert('Atenção!', 'Por favor, preencha todos os campos obriatórios antes de salvar.');
-      return;
-    }
+  // Hooks
+  const {
+    control,
+    handleSubmit,
+    reset,
+    setValue
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  // Functions
+  const handleSaveItem = ({ name }: FormSchemaType) => {
     database
-      .collection('Task')
+      .collection("Task")
       .doc()
       .set({
         name,
         uid: uid,
-        createdAt: formattedDate
+        createdAt: formattedDate,
       })
       .then(() => {
-        Toast.show('Item adicionado!', { type: 'success' });
-        setName('')
-        onCloseModal && onCloseModal();
-      })
-      .catch(error => {
-        console.error('Erro ao adicionar o item: ', error);
-      });
-  }
-
-  const handleDeleteExpense = () => {
-    if (!selectedItemId) {
-      console.error('Nenhum documento selecionado para exclusão!');
-      return;
-    }
-
-    const expenseRef = database.collection('Marketplace').doc(selectedItemId);
-    expenseRef.delete()
-      .then(() => {
-        Toast.show('Item Excluido!', { type: 'success' });
+        Toast.show("Item adicionado!", { type: "success" });
+        reset()
         onCloseModal && onCloseModal();
       })
       .catch((error) => {
-        console.error('Erro ao excluir o documento de item:', error);
+        console.error("Erro ao adicionar o item: ", error);
       });
   };
 
-  const handleEditExpense = () => {
+  const handleDeleteExpense = () => {
     if (!selectedItemId) {
-      console.error('Nenhum documento selecionado para edição!');
+      console.error("Nenhum documento selecionado para exclusão!");
       return;
     }
 
+    const expenseRef = database.collection("Marketplace").doc(selectedItemId);
+    expenseRef
+      .delete()
+      .then(() => {
+        Toast.show("Item Excluido!", { type: "success" });
+        onCloseModal && onCloseModal();
+      })
+      .catch((error) => {
+        console.error("Erro ao excluir o documento de item:", error);
+      });
+  };
 
+  const handleEditExpense = ({ name }: FormSchemaType) => {
     database
-      .collection('Task')
+      .collection("Task")
       .doc(selectedItemId)
       .set({
         name,
         uid: uid,
       })
       .then(() => {
-        Toast.show('Item adicionado!', { type: 'success' });
-        setName('')
+        Toast.show("Item adicionado!", { type: "success" });
+        reset();
         onCloseModal && onCloseModal();
+        !!closeBottomSheet && closeBottomSheet()
       })
-      .catch(error => {
-        console.error('Erro ao adicionar o item: ', error);
+      .catch((error) => {
+        console.error("Erro ao adicionar o item: ", error);
       });
   };
 
+  const onInvalid = () => {
+    Alert.alert(isEditing ? "Nenhum documento selecionado para edição!" : 'Atenção!', 'Por favor, preencha todos os campos obriatórios antes de salvar.');
+  }
+
   useEffect(() => {
     if (selectedItemId) {
-      database.collection('Task').doc(selectedItemId).get().then((doc) => {
-        if (doc.exists) {
-          const data = doc.data();
-          if (data) {
-            setName(data.name);
-            setIsEditing(true);
+      database
+        .collection("Task")
+        .doc(selectedItemId)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            const data = doc.data();
+            if (data) {
+              setValue("name", data.name);
+              setIsEditing(true);
+            } else {
+              console.log("Dados do documento estão vazios!");
+            }
           } else {
-            console.log('Dados do documento estão vazios!');
+            console.log("Nenhum documento encontrado!");
           }
-        } else {
-          console.log('Nenhum documento encontrado!');
-        }
-      }).catch((error) => {
-        console.error('Erro ao obter o documento:', error);
-      });
+        })
+        .catch((error) => {
+          console.error("Erro ao obter o documento:", error);
+        });
     }
   }, [selectedItemId]);
 
   return (
     <>
       <DefaultContainer hasHeader={false}>
-      <ButtonClose onPress={closeBottomSheet} style={{alignSelf: "flex-end", marginBottom: 32}}>
-          <Title style={{ color: 'white' }}>Fechar</Title>
+        <ButtonClose
+          onPress={closeBottomSheet}
+          style={{ alignSelf: "flex-end", marginBottom: 32 }}
+        >
+          <Title style={{ color: "white" }}>Fechar</Title>
         </ButtonClose>
-        <Container title={'Adicionar nova tarefa'}>
-        <ScrollView
-        keyboardShouldPersistTaps="always"
-        showsVerticalScrollIndicator={false}
-      >
-        <Content>
-          <Title>
-            Nome da tarefa
-          </Title>
-          <Input
-            value={name}
-            onChangeText={setName}
-          />
-        
-          <View style={{ marginBottom: 10, height: 150 }}>
-            {showButtonSave && (
-              <Button style={{ marginBottom: 10 }} onPress={isEditing ? handleEditExpense : handleSaveItem}>
-                <Title>{isEditing ? 'Editar' : 'Salvar'}</Title>
-              </Button>
-            )}
-            {showButtonRemove && (
-              <Button style={{ marginBottom: 10 }} onPress={handleDeleteExpense}>
-                <Title>Excluir</Title>
-              </Button>
-            )}
-          </View>
-        </Content>
-      </ScrollView>
+        <Container title={"Adicionar nova tarefa"}>
+          <ScrollView
+            keyboardShouldPersistTaps="always"
+            showsVerticalScrollIndicator={false}
+          >
+            <Content>
+              <Title>Nome da tarefa</Title>
+              <Controller
+                control={control}
+                name="name"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <Input onBlur={onBlur} onChangeText={onChange} value={value} />
+                )}
+              />
+
+              <View style={{ marginBottom: 10, height: 150 }}>
+                {showButtonSave && (
+                  <Button
+                    style={{ marginBottom: 10 }}
+                    onPress={isEditing ? handleSubmit(handleEditExpense, onInvalid) : handleSubmit(handleSaveItem, onInvalid)}
+                  >
+                    <Title>{isEditing ? "Editar" : "Salvar"}</Title>
+                  </Button>
+                )}
+                {showButtonRemove && (
+                  <Button
+                    style={{ marginBottom: 10 }}
+                    onPress={handleDeleteExpense}
+                  >
+                    <Title>Excluir</Title>
+                  </Button>
+                )}
+              </View>
+            </Content>
+          </ScrollView>
         </Container>
       </DefaultContainer>
     </>
