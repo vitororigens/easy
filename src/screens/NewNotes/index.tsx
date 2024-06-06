72ad460c4f73d76b6c1b45025711734710b288b9
devjpedro <joaopedross.ti9@gmail.com>
2024-06-05 22:54:31 -0300

    newNotes(optimize): adicionando react hook form no formulário de adicionar/editar nota

--- src/screens/NewNotes/index.tsx
+++ src/screens/NewNotes/index.tsx
@@ -1,11 +1,21 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { Alert, ScrollView, View } from "react-native";
 import { Toast } from "react-native-toast-notifications";
+import { z } from "zod";
 import { Container } from "../../components/Container";
 import { DefaultContainer } from "../../components/DefaultContainer";
 import { useUserAuth } from "../../hooks/useUserAuth";
-import { database } from '../../services';
-import { Button, ButtonClose, Content, Input, InputContainer, Title } from "./styles";
+import { database } from "../../services";
+import {
+  Button,
+  ButtonClose,
+  Content,
+  Input,
+  InputContainer,
+  Title,
+} from "./styles";
 
 type Props = {
   closeBottomSheet?: () => void;
@@ -14,150 +24,202 @@ type Props = {
   showButtonSave?: boolean;
   showButtonRemove?: boolean;
   selectedItemId?: string;
-}
+};
 
-export function NewNotes({ closeBottomSheet, onCloseModal, showButtonEdit, showButtonSave, showButtonRemove, selectedItemId }: Props) {
-  const [name, setName] = useState('')
-  const [description, setDescription] = useState('');
+const formSchema = z.object({
+  name: z.string().min(1, "Nome da Tarefa é obrigatória"),
+  description: z.string().min(1, "Descrição é obrigatória"),
+});
+
+type FormSchemaType = z.infer<typeof formSchema>;
+
+export function NewNotes({
+  closeBottomSheet,
+  onCloseModal,
+  showButtonEdit,
+  showButtonSave,
+  showButtonRemove,
+  selectedItemId,
+}: Props) {
+  // States
   const user = useUserAuth();
   const uid = user?.uid;
   const [isEditing, setIsEditing] = useState(false);
-  console.log(uid)
 
+  // Hooks
+  const { control, handleSubmit, reset, setValue } = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: "",
+      name: "",
+    },
+  });
 
-  const handleSaveItem = () => {
-    if (name === '') {
-      Alert.alert('Atenção!', 'Por favor, preencha todos os campos obriatórios antes de salvar.');
-      return;
-    }
+  // Functions
+  const handleSaveItem = ({ name, description }: FormSchemaType) => {
     database
-      .collection('Notes')
+      .collection("Notes")
       .doc()
       .set({
         name,
+        description,
         uid: uid,
-        description: description
       })
       .then(() => {
-        Toast.show('Nota adicionado!', { type: 'success' });
-        setName('')
+        Toast.show("Nota adicionada!", { type: "success" });
+        reset();
         onCloseModal && onCloseModal();
-        !!closeBottomSheet && closeBottomSheet()
-      })
-      .catch(error => {
-        console.error('Erro ao adicionar o item: ', error);
-      });
-  }
-
-
-  const handleDeleteExpense = () => {
-    if (!selectedItemId) {
-      console.error('Nenhum documento selecionado para exclusão!');
-      return;
-    }
-
-    const expenseRef = database.collection('Notes').doc(selectedItemId);
-    expenseRef.delete()
-      .then(() => {
-        Toast.show('Nota Excluída!', { type: 'success' });
-        onCloseModal && onCloseModal();
-        !!closeBottomSheet && closeBottomSheet()
+        !!closeBottomSheet && closeBottomSheet();
       })
       .catch((error) => {
-        console.error('Erro ao excluir o documento de item:', error);
+        console.error("Erro ao adicionar o item: ", error);
       });
   };
 
-  const handleEditExpense = () => {
+  const handleDeleteExpense = () => {
     if (!selectedItemId) {
-      console.error('Nenhum documento selecionado para edição!');
+      console.error("Nenhum documento selecionado para exclusão!");
       return;
     }
 
+    const expenseRef = database.collection("Notes").doc(selectedItemId);
+    expenseRef
+      .delete()
+      .then(() => {
+        Toast.show("Nota Excluída!", { type: "success" });
+        onCloseModal && onCloseModal();
+        !!closeBottomSheet && closeBottomSheet();
+      })
+      .catch((error) => {
+        console.error("Erro ao excluir o documento de item:", error);
+      });
+  };
+
+  const handleEditExpense = ({ name, description }: FormSchemaType) => {
+    if (!selectedItemId) {
+      console.error("Nenhum documento selecionado para edição!");
+      return;
+    }
 
     database
-      .collection('Notes')
+      .collection("Notes")
       .doc(selectedItemId)
       .set({
         name,
         uid,
-        description
+        description,
       })
       .then(() => {
-        Toast.show('Nota adicionado!', { type: 'success' });
-        setName('')
+        Toast.show("Nota editada!", { type: "success" });
+        reset();
         onCloseModal && onCloseModal();
+        !!closeBottomSheet && closeBottomSheet();
       })
-      .catch(error => {
-        console.error('Erro ao adicionar o item: ', error);
+      .catch((error) => {
+        console.error("Erro ao adicionar o item: ", error);
       });
   };
 
+  const onInvalid = () => {
+    Alert.alert(
+      "Atenção!",
+      "Por favor, preencha todos os campos obriatórios antes de salvar."
+    );
+  };
 
   useEffect(() => {
     if (selectedItemId) {
-      database.collection('Notes').doc(selectedItemId).get().then((doc) => {
-        if (doc.exists) {
-          const data = doc.data();
-          if (data) {
-            setName(data.name);
-            setDescription(data.description)
-            setIsEditing(true);
+      database
+        .collection("Notes")
+        .doc(selectedItemId)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            const data = doc.data();
+            if (data) {
+              setValue("name", data.name);
+              setValue("description", data.description);
+              setIsEditing(true);
+            } else {
+              console.log("Dados do documento estão vazios!");
+            }
           } else {
-            console.log('Dados do documento estão vazios!');
+            console.log("Nenhum documento encontrado!");
           }
-        } else {
-          console.log('Nenhum documento encontrado!');
-        }
-      }).catch((error) => {
-        console.error('Erro ao obter o documento:', error);
-      });
+        })
+        .catch((error) => {
+          console.error("Erro ao obter o documento:", error);
+        });
     }
   }, [selectedItemId]);
 
   return (
     <>
       <DefaultContainer hasHeader={false}>
-      <ButtonClose onPress={closeBottomSheet} style={{alignSelf: "flex-end", marginBottom: 32}}>
-          <Title style={{ color: 'white' }}>Fechar</Title>
+        <ButtonClose
+          onPress={closeBottomSheet}
+          style={{ alignSelf: "flex-end", marginBottom: 32 }}
+        >
+          <Title style={{ color: "white" }}>Fechar</Title>
         </ButtonClose>
-        <Container title={'Adicionar nova nota'}>
-        <ScrollView
-        keyboardShouldPersistTaps="always"
-        showsVerticalScrollIndicator={false}
-      >
-        <Content>
-          <Title>
-            Nome da nota
-          </Title>
-          <Input
-            value={name}
-            onChangeText={setName}
-          />
-          <Title>
-            Nota
-          </Title>
-          <InputContainer
-          multiline
-          numberOfLines={20}
-          value={description}
-          onChangeText={setDescription}
-          textAlignVertical="top"
-          />
-          <View style={{ marginBottom: 10, height: 150 }}>
-            {showButtonSave && (
-              <Button style={{ marginBottom: 10 }} onPress={isEditing ? handleEditExpense : handleSaveItem}>
-                <Title>{isEditing ? 'Editar' : 'Salvar'}</Title>
-              </Button>
-            )}
-            {showButtonRemove && (
-              <Button style={{ marginBottom: 10 }} onPress={handleDeleteExpense}>
-                <Title>Excluir</Title>
-              </Button>
-            )}
-          </View>
-        </Content>
-      </ScrollView>
+        <Container title={"Adicionar nova nota"}>
+          <ScrollView
+            keyboardShouldPersistTaps="always"
+            showsVerticalScrollIndicator={false}
+          >
+            <Content>
+              <Title>Nome da nota</Title>
+              <Controller
+                control={control}
+                name="name"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <Input
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                  />
+                )}
+              />
+              <Title>Nota</Title>
+              <Controller
+                control={control}
+                name="description"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <InputContainer
+                    multiline
+                    numberOfLines={20}
+                    value={value}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    textAlignVertical="top"
+                  />
+                )}
+              />
+
+              <View style={{ marginBottom: 10, height: 150 }}>
+                {showButtonSave && (
+                  <Button
+                    style={{ marginBottom: 10 }}
+                    onPress={
+                      isEditing
+                        ? handleSubmit(handleEditExpense, onInvalid)
+                        : handleSubmit(handleSaveItem, onInvalid)
+                    }
+                  >
+                    <Title>{isEditing ? "Editar" : "Salvar"}</Title>
+                  </Button>
+                )}
+                {showButtonRemove && (
+                  <Button
+                    style={{ marginBottom: 10 }}
+                    onPress={handleDeleteExpense}
+                  >
+                    <Title>Excluir</Title>
+                  </Button>
+                )}
+              </View>
+            </Content>
+          </ScrollView>
         </Container>
       </DefaultContainer>
     </>
