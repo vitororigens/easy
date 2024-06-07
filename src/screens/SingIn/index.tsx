0a0f6fe16861e43d7a3ac5ae9cff4223a40006ec
devjpedro <joaopedross.ti9@gmail.com>
2024-06-07 19:40:41 -0300

    login(optimize): adicionando react hook form no formulário de login

--- src/screens/SingIn/index.tsx
+++ src/screens/SingIn/index.tsx
@@ -1,65 +1,99 @@
 import auth from "@react-native-firebase/auth";
 import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
 import { Toast } from "react-native-toast-notifications";
 //
-import { ButtonForgetPassword, Container, Content, ContentIcon, Divider, Span, SubTitle, Text, Title } from "./styles";
+import { ButtonForgetPassword, Container, Span, Text, Title } from "./styles";
 //
-import { ButtonIcon } from "../../components/ButtonIcon";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 
+const formSchema = z.object({
+  email: z.string().email("Formato inválido").min(1, "Email é obrigatório"),
+  password: z.string().min(1, "Senha é obrigatória"),
+});
+
+type FormSchemaType = z.infer<typeof formSchema>;
+
 export function SingIn() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigation = useNavigation()
-    
-    function handleForgetPassword(){
-        navigation.navigate('forgetPassword')
-    }
-    function handleSingIn() {
-        if (!email || !password) {
-            Toast.show('Por favor, preencha todos os campos.', { type: 'danger' });
-            return;
-        }
+  const navigation = useNavigation();
 
-        auth()
-            .signInWithEmailAndPassword(email.trim(), password.trim())
-            .then(() => {
-                Toast.show('Login realizado com sucesso!', { type: 'success' })
-                setEmail("")
-                setPassword("")
-                navigation.navigate('tabroutes')
+  // Hooks
+  const { control, handleSubmit, reset } = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-            })
-            .catch(() => Toast.show('Verifique se seu e-mail ou senha estão corretos.', { type: 'danger' }))
-    }
-    return (
-        <Container>
-            <Title>
-                Entre e faça o controle de suas <Span>finanças pessoais</Span>.
-            </Title>
-            <Input name="envelope" value={email} onChangeText={setEmail} showIcon placeholder="Email" />
-            <Input name="lock" value={password} onChangeText={setPassword} showIcon placeholder="Senha" passwordType />
-            <Button title={'Entrar'} onPress={handleSingIn} />
-            <ButtonForgetPassword onPress={handleForgetPassword}>
-                <Text>
-                    Esqueceu a senha?
-                </Text>
-            </ButtonForgetPassword>
-            {/* <Content>
-                <Divider />
-                <SubTitle>
-                    Conect-se
-                </SubTitle>
-                <Divider />
-            </Content>
-            <ContentIcon>
-                <ButtonIcon name="microsoft" color="#00a1f1" />
-                <ButtonIcon name="apple" color="#7200b5" />
-                <ButtonIcon name="google" color="#dc3c2a" />
-                <ButtonIcon name="facebook" color="#0863f7" />
-            </ContentIcon> */}
-        </Container>
-    )
+  // Functions
+  function handleForgetPassword() {
+    navigation.navigate("forgetPassword");
+  }
+  function handleSingIn({ email, password }: FormSchemaType) {
+    auth()
+      .signInWithEmailAndPassword(email.trim(), password.trim())
+      .then(() => {
+        Toast.show("Login realizado com sucesso!", { type: "success" });
+        reset();
+        navigation.navigate("tabroutes");
+      })
+      .catch(() =>
+        Toast.show("Verifique se seu e-mail ou senha estão corretos.", {
+          type: "danger",
+        })
+      );
+  }
+
+  const onInvalid = () => {
+    Toast.show("Por favor, preencha todos os campos.", { type: "danger" });
+  };
+
+  return (
+    <Container>
+      <Title>
+        Entre e faça o controle de suas <Span>finanças pessoais</Span>.
+      </Title>
+      <Controller
+        control={control}
+        name="email"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            name="envelope"
+            value={value}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            showIcon
+            placeholder="Email"
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="password"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Input
+            name="lock"
+            value={value}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            showIcon
+            placeholder="Senha"
+            passwordType
+          />
+        )}
+      />
+
+      <Button
+        title={"Entrar"}
+        onPress={handleSubmit(handleSingIn, onInvalid)}
+      />
+      <ButtonForgetPassword onPress={handleForgetPassword}>
+        <Text>Esqueceu a senha?</Text>
+      </ButtonForgetPassword>
+    </Container>
+  );
 }
