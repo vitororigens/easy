ac4b85d92062efb3bf4fa467f5a87d92a88a1708
devjpedro <joaopedross.ti9@gmail.com>
2024-06-07 20:53:09 -0300

    marketplace(optimize): adicionando react hook form no formulário de adicionar item no carrinho

--- src/screens/NewItem/index.tsx
+++ src/screens/NewItem/index.tsx
@@ -1,11 +1,15 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { Alert, ScrollView, View } from "react-native";
-import RNPickerSelect from 'react-native-picker-select';
+import RNPickerSelect from "react-native-picker-select";
 import { Toast } from "react-native-toast-notifications";
+import { z } from "zod";
 import { Container } from "../../components/Container";
 import { DefaultContainer } from "../../components/DefaultContainer";
 import { useUserAuth } from "../../hooks/useUserAuth";
-import { database } from '../../services';
+import { database } from "../../services";
+import { currencyMask, currencyUnMask } from "../../utils/currency";
 import { Button, ButtonClose, Content, Input, Span, Title } from "./styles";
 
 type Props = {
@@ -15,249 +19,344 @@ type Props = {
   showButtonSave?: boolean;
   showButtonRemove?: boolean;
   selectedItemId?: string;
-}
+};
 
-export function NewItem({ closeBottomSheet, onCloseModal, showButtonEdit, showButtonSave, showButtonRemove, selectedItemId }: Props) {
-  const [selectedCategory, setSelectedCategory] = useState('geral');
-  const [selectedMeasurements, setSelectedMeasurements] = useState('');
-  const [valueItem, setValueItem] = useState('0.00');
-  const [name, setName] = useState('')
-  const [amount, setAmount] = useState('1')
-  const [description, setDescription] = useState('');
+const formSchema = z.object({
+  name: z.string().min(1, "Nome da Tarefa é obrigatório"),
+  amount: z.string().optional(),
+  valueItem: z.string().optional(),
+  selectedCategory: z.string().optional(),
+  selectedMeasurements: z.string().optional(),
+  description: z.string().optional(),
+});
+
+type FormSchemaType = z.infer<typeof formSchema>;
+
+export function NewItem({
+  closeBottomSheet,
+  onCloseModal,
+  showButtonEdit,
+  showButtonSave,
+  showButtonRemove,
+  selectedItemId,
+}: Props) {
   const user = useUserAuth();
   const uid = user?.uid;
+
+  // States
   const [isEditing, setIsEditing] = useState(false);
-  console.log(uid)
 
+  // Hooks
+  const { control, handleSubmit, reset, setValue, formState: {errors} } = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: "",
+      name: "",
+      selectedCategory: "",
+      amount: "",
+      valueItem: "",
+      selectedMeasurements: "un",
+    },
+  });
 
+  // Functions
   function formatQuantity(quantity: string) {
-    const formattedQuantity = quantity.replace(/\D/g, '').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    const formattedQuantity = quantity
+      .replace(/\D/g, "")
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
     return formattedQuantity;
   }
 
-
-  const handleSaveItem = () => {
-    if (name === '') {
-      Alert.alert('Atenção!', 'Por favor, preencha todos os campos obriatórios antes de salvar.');
-      return;
-    }
+  const handleSaveItem = ({
+    name,
+    amount,
+    description,
+    selectedCategory,
+    selectedMeasurements,
+    valueItem,
+  }: FormSchemaType) => {
     database
-      .collection('Marketplace')
+      .collection("Marketplace")
       .doc()
       .set({
-        category: selectedCategory,
+        category: selectedCategory ? selectedCategory : "geral",
         measurements: selectedMeasurements,
-        valueItem: parseFloat(valueItem as string),
+        valueItem: valueItem ? currencyUnMask(valueItem) : 0,
         name,
-        amount: parseFloat(amount as string),
+        amount: amount ? parseFloat(amount as string) : 1,
         description,
-        uid: uid,
+        uid,
       })
       .then(() => {
-        Toast.show('Item adicionado!', { type: 'success' });
-        setSelectedCategory('')
-        setAmount('')
-        setDescription('')
-        setName('')
-        setSelectedMeasurements('')
-        setValueItem('')
-        onCloseModal && onCloseModal();
-      })
-      .catch(error => {
-        console.error('Erro ao adicionar o item: ', error);
-      });
-  }
-
-
-  const handleDeleteExpense = () => {
-    if (!selectedItemId) {
-      console.error('Nenhum documento selecionado para exclusão!');
-      return;
-    }
-
-    const expenseRef = database.collection('Marketplace').doc(selectedItemId);
-    expenseRef.delete()
-      .then(() => {
-        Toast.show('Item Excluido!', { type: 'success' });
+        Toast.show("Item adicionado!", { type: "success" });
+        reset();
         onCloseModal && onCloseModal();
       })
       .catch((error) => {
-        console.error('Erro ao excluir o documento de item:', error);
+        console.error("Erro ao adicionar o item: ", error);
       });
   };
 
-  const handleEditExpense = () => {
+  const handleDeleteExpense = () => {
     if (!selectedItemId) {
-      console.error('Nenhum documento selecionado para edição!');
+      console.error("Nenhum documento selecionado para exclusão!");
       return;
     }
 
-
-    database
-      .collection('Marketplace')
-      .doc(selectedItemId)
-      .set({
-        category: selectedCategory,
-        measurements: selectedMeasurements,
-        valueItem: parseFloat(valueItem as string),
-        name,
-        amount: parseFloat(amount as string),
-        description,
-        uid: uid,
-      })
+    const expenseRef = database.collection("Marketplace").doc(selectedItemId);
+    expenseRef
+      .delete()
       .then(() => {
-        Toast.show('Item adicionado!', { type: 'success' });
-        setSelectedCategory('')
-        setAmount('')
-        setDescription('')
-        setName('')
-        setSelectedMeasurements('')
-        setValueItem('')
+        Toast.show("Item Excluido!", { type: "success" });
         onCloseModal && onCloseModal();
       })
-      .catch(error => {
-        console.error('Erro ao adicionar o item: ', error);
+      .catch((error) => {
+        console.error("Erro ao excluir o documento de item:", error);
       });
   };
 
+  const handleEditExpense = ({
+    name,
+    amount,
+    description,
+    selectedCategory,
+    selectedMeasurements,
+    valueItem,
+  }: FormSchemaType) => {
+    if (!selectedItemId) {
+      console.error("Nenhum documento selecionado para edição!");
+      return;
+    }
+
+    database
+      .collection("Marketplace")
+      .doc(selectedItemId)
+      .set({
+        category: selectedCategory ? selectedCategory : "geral",
+        measurements: selectedMeasurements,
+        valueItem: valueItem ? currencyUnMask(valueItem) : 0,
+        name,
+        amount: amount ? parseFloat(amount as string) : 1,
+        description,
+        uid,
+      })
+      .then(() => {
+        Toast.show("Item adicionado!", { type: "success" });
+        reset();
+        onCloseModal && onCloseModal();
+      })
+      .catch((error) => {
+        console.error("Erro ao adicionar o item: ", error);
+      });
+  };
+
+  const onInvalid = () => {
+    Alert.alert(
+      "Atenção!",
+      "Por favor, preencha todos os campos obriatórios antes de salvar."
+    );
+  };
 
   useEffect(() => {
     if (selectedItemId) {
-      database.collection('Marketplace').doc(selectedItemId).get().then((doc) => {
-        if (doc.exists) {
-          const data = doc.data();
-          if (data) {
-            setSelectedCategory(data.category);
-            setSelectedMeasurements(data.measurements);
-            setValueItem(data.valueItem);
-            setDescription(data.description);
-            setName(data.name);
-            setAmount(data.amount.toString());
-            setIsEditing(true);
+      database
+        .collection("Marketplace")
+        .doc(selectedItemId)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            const data = doc.data();
+            if (data) {
+              setValue("name", data.name);
+              setValue("selectedCategory", data.category);
+              setValue("selectedMeasurements", data.measurements);
+              setValue("valueItem", currencyMask(data.valueItem.toString()));
+              setValue("amount", data.amount.toString());
+              setIsEditing(true);
+            } else {
+              console.log("Dados do documento estão vazios!");
+            }
           } else {
-            console.log('Dados do documento estão vazios!');
+            console.log("Nenhum documento encontrado!");
           }
-        } else {
-          console.log('Nenhum documento encontrado!');
-        }
-      }).catch((error) => {
-        console.error('Erro ao obter o documento:', error);
-      });
+        })
+        .catch((error) => {
+          console.error("Erro ao obter o documento:", error);
+        });
     }
   }, [selectedItemId]);
 
   return (
     <DefaultContainer hasHeader={false}>
-      <ButtonClose onPress={closeBottomSheet} style={{alignSelf: "flex-end", marginBottom: 32}}>
-          <Title style={{ color: 'white' }}>Fechar</Title>
-        </ButtonClose>
+      <ButtonClose
+        onPress={closeBottomSheet}
+        style={{ alignSelf: "flex-end", marginBottom: 32 }}
+      >
+        <Title style={{ color: "white" }}>Fechar</Title>
+      </ButtonClose>
       <Container>
         <ScrollView
           keyboardShouldPersistTaps="always"
           showsVerticalScrollIndicator={false}
         >
           <Content>
-            <Title>
-              Nome*
-            </Title>
-            <Input
-              value={name}
-              onChangeText={setName}
+            <Title>Nome*</Title>
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input onBlur={onBlur} onChangeText={onChange} value={value} />
+              )}
             />
-            <View style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              width: '100%'
-            }}>
-              <View style={{
-                width: '50%',
-                paddingRight: 15
-              }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: "100%",
+              }}
+            >
+              <View
+                style={{
+                  width: "50%",
+                  paddingRight: 15,
+                }}
+              >
                 <Title>
                   Quantidade<Span> (opicional)</Span>
                 </Title>
-                <Input
-                  value={formatQuantity(amount)}
-                  keyboardType="numeric"
-                  onChangeText={setAmount}
+                <Controller
+                  control={control}
+                  name="amount"
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <Input
+                      keyboardType="numeric"
+                      value={formatQuantity(value ?? "")}
+                      onChangeText={onChange}
+                      onBlur={onBlur}
+                    />
+                  )}
                 />
-
               </View>
-              <View style={{
-                width: 100
-              }}>
-                <RNPickerSelect
-                  onValueChange={(value) => setSelectedMeasurements(value)}
-                  items={[
-                    { label: 'un', value: 'unidade' },
-                    { label: 'kg', value: 'kilo' },
-                    { label: 'g', value: 'gramas' },
-                    { label: 'l', value: 'litro' },
-                    { label: 'ml', value: 'milimetros' },
-
-
-                  ]}
-                  value={selectedMeasurements}
-                  placeholder={{ label: 'un', value: 'un' }}
+              <View
+                style={{
+                  width: 100,
+                }}
+              >
+                <Controller
+                  control={control}
+                  name="selectedMeasurements"
+                  render={({ field: { onChange, value } }) => (
+                    <RNPickerSelect
+                      onValueChange={onChange}
+                      items={[
+                        { label: "un", value: "unidade" },
+                        { label: "kg", value: "kilo" },
+                        { label: "g", value: "gramas" },
+                        { label: "l", value: "litro" },
+                        { label: "ml", value: "milimetros" },
+                      ]}
+                      value={value}
+                      placeholder={{ label: "un", value: "un" }}
+                    />
+                  )}
                 />
               </View>
             </View>
             <Title>
               Preço <Span> (opicional)</Span>
             </Title>
-            <Input
-              placeholder="0,00"
-              value={valueItem}
-              keyboardType="numeric"
-              onChangeText={setValueItem}
+            <Controller
+              control={control}
+              name="valueItem"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input
+                  value={value}
+                  onChangeText={(value) => onChange(currencyMask(value))}
+                  onBlur={onBlur}
+                  placeholder="0,00"
+                  keyboardType="numeric"
+                />
+              )}
             />
-
 
             <Title>
               Categoria <Span> (opicional)</Span>
             </Title>
-            <RNPickerSelect
-              onValueChange={(value) => setSelectedCategory(value)}
-              items={[
-                { label: 'Ovos, Verduras e Frutas', value: 'ovos, verduras e frutas' },
-                { label: 'Carnes', value: 'carnes' },
-                { label: 'Laticínios', value: 'laticinios' },
-                { label: 'Padaria', value: 'padaria' },
-                { label: 'Bebidas', value: 'bebidas' },
-                { label: 'Produtos de Limpeza', value: 'produtos de limpeza' },
-                { label: 'Higiene Pessoal', value: 'higiene pessoal' },
-                { label: 'Hortifruti', value: 'hortifruti' },
-                { label: 'Congelados', value: 'congelados' },
-                { label: 'Açougue', value: 'acougue' },
-                { label: 'Peixaria', value: 'peixaria' },
-                { label: 'Bebidas Alcoólicas', value: 'bebidas alcoolicas' },
-                { label: 'Pet Shop', value: 'pet shop' },
-                { label: 'Utensílios Domésticos', value: 'utensilios domesticos' },
-                { label: 'Bebês e Crianças', value: 'bebes e criancas' },
-                { label: 'Eletronicos', value: 'Eletronicos' },
-                { label: 'Carro', value: 'Carro' },
-                { label: 'Ferramentas', value: 'Ferramentas' },
-              ]}
-              value={selectedCategory}
-              placeholder={{ label: 'Selecione', value: 'Selecione' }}
+
+            <Controller
+              control={control}
+              name="selectedCategory"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <RNPickerSelect
+                  onValueChange={onChange}
+                  items={[
+                    {
+                      label: "Ovos, Verduras e Frutas",
+                      value: "ovos, verduras e frutas",
+                    },
+                    { label: "Carnes", value: "carnes" },
+                    { label: "Laticínios", value: "laticinios" },
+                    { label: "Padaria", value: "padaria" },
+                    { label: "Bebidas", value: "bebidas" },
+                    {
+                      label: "Produtos de Limpeza",
+                      value: "produtos de limpeza",
+                    },
+                    { label: "Higiene Pessoal", value: "higiene pessoal" },
+                    { label: "Hortifruti", value: "hortifruti" },
+                    { label: "Congelados", value: "congelados" },
+                    { label: "Açougue", value: "acougue" },
+                    { label: "Peixaria", value: "peixaria" },
+                    {
+                      label: "Bebidas Alcoólicas",
+                      value: "bebidas alcoolicas",
+                    },
+                    { label: "Pet Shop", value: "pet shop" },
+                    {
+                      label: "Utensílios Domésticos",
+                      value: "utensilios domesticos",
+                    },
+                    { label: "Bebês e Crianças", value: "bebes e criancas" },
+                    { label: "Eletronicos", value: "Eletronicos" },
+                    { label: "Carro", value: "Carro" },
+                    { label: "Ferramentas", value: "Ferramentas" },
+                  ]}
+                  value={value}
+                  placeholder={{ label: "Selecione", value: "Selecione" }}
+                />
+              )}
             />
 
             <Title>
               Observação <Span> (opicional)</Span>
             </Title>
-            <Input
-              value={description}
-              onChangeText={setDescription}
+            <Controller
+              control={control}
+              name="description"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Input value={value} onChangeText={onChange} onBlur={onBlur} />
+              )}
             />
+
             <View style={{ marginBottom: 10, height: 150 }}>
               {showButtonSave && (
-                <Button style={{ marginBottom: 10 }} onPress={isEditing ? handleEditExpense : handleSaveItem}>
-                  <Title>{isEditing ? 'Editar' : 'Salvar'}</Title>
+                <Button
+                  style={{ marginBottom: 10 }}
+                  onPress={
+                    isEditing
+                      ? handleSubmit(handleEditExpense, onInvalid)
+                      : handleSubmit(handleSaveItem, onInvalid)
+                  }
+                >
+                  <Title>{isEditing ? "Editar" : "Salvar"}</Title>
                 </Button>
               )}
               {showButtonRemove && (
-                <Button style={{ marginBottom: 10 }} onPress={handleDeleteExpense}>
+                <Button
+                  style={{ marginBottom: 10 }}
+                  onPress={handleDeleteExpense}
+                >
                   <Title>Excluir</Title>
                 </Button>
               )}
