82f49cba51c2f79774e4f2b5248ca15d1efada3f
devjpedro <joaopedross.ti9@gmail.com>
2024-06-07 21:44:32 -0300

    notes(adicao): filtrando por mes as notas

--- src/screens/Notes/index.tsx
+++ src/screens/Notes/index.tsx
@@ -1,23 +1,33 @@
-import { useState } from 'react';
-import { Dimensions, FlatList, Modal, ScrollView } from 'react-native';
-import { Toast } from 'react-native-toast-notifications';
-import { Container } from '../../components/Container';
-import { DefaultContainer } from '../../components/DefaultContainer';
-import { ItemNotes } from '../../components/ItemNotes';
-import { LoadData } from '../../components/LoadData';
-import useMarketplaceCollections from '../../hooks/useMarketplaceCollections';
-import { useUserAuth } from '../../hooks/useUserAuth';
-import { database } from '../../services';
-import { NewNotes } from '../NewNotes';
+import { getMonth, parse } from "date-fns";
+import { useState } from "react";
+import { Dimensions, FlatList, Modal } from "react-native";
+import { Toast } from "react-native-toast-notifications";
+import { Container } from "../../components/Container";
+import { DefaultContainer } from "../../components/DefaultContainer";
+import { ItemNotes } from "../../components/ItemNotes";
+import { LoadData } from "../../components/LoadData";
+import { useMonth } from "../../context/MonthProvider";
+import useMarketplaceCollections from "../../hooks/useMarketplaceCollections";
+import { useUserAuth } from "../../hooks/useUserAuth";
+import { database } from "../../services";
+import { NewNotes } from "../NewNotes";
 
-const screenWidth = Dimensions.get('screen').width;
+const screenWidth = Dimensions.get("screen").width;
 
 export function Notes() {
+  const { selectedMonth } = useMonth();
   const [showNotesModal, setShowNotesModal] = useState(false);
-  const [selectedItemId, setSelectedItemId] = useState('');
-  const data = useMarketplaceCollections('Notes');
+  const [selectedItemId, setSelectedItemId] = useState("");
+  const data = useMarketplaceCollections("Notes");
   const user = useUserAuth();
   const uid = user?.uid;
+  const notesUser = data.filter((item) => item.uid === uid);
+
+  const notesUserMonth = notesUser.filter((item) => {
+    const month =
+      getMonth(parse(item.createdAt ?? "", "dd/MM/yyyy", new Date())) + 1;
+    return month === selectedMonth;
+  });
 
   function closeModals() {
     setShowNotesModal(false);
@@ -29,38 +39,41 @@ export function Notes() {
   }
 
   function handleDeleteItem(documentId: string) {
-    database.collection('Notes').doc(documentId).delete()
+    database
+      .collection("Notes")
+      .doc(documentId)
+      .delete()
       .then(() => {
-        Toast.show('Nota excluída!', { type: 'success' });
+        Toast.show("Nota excluída!", { type: "success" });
       })
-      .catch(error => {
-        console.error('Erro ao excluir a nota: ', error);
+      .catch((error) => {
+        console.error("Erro ao excluir a nota: ", error);
       });
   }
 
-
   return (
-    <DefaultContainer newNotes>
+    <DefaultContainer newNotes monthButton>
       <Container type="SECONDARY" title="Notas">
-        {data.filter(item => item.uid === uid).length === 0 ? (
-          <ScrollView>
-            <LoadData image='PRIMARY' title='Desculpe!' subtitle='Você ainda não possui dados para exibir aqui! Comece adicionando uma nova anotação clicando em Novo +.' />
-          </ScrollView>
-        ) : (
-          <FlatList
-            data={data.filter(item => item.uid === uid)}
-            renderItem={({ item }) => (
-              <ItemNotes
-                onDelete={() => handleDeleteItem(item.id)}
-                onEdit={() => handleEditItem(item.id)}
-                title={item.name}
-                date={item.createdAt}
-                description={item.description}
-              />
-            )}
-            keyExtractor={(item) => item.id}
-          />
-        )}
+        <FlatList
+          data={notesUserMonth}
+          renderItem={({ item }) => (
+            <ItemNotes
+              onDelete={() => handleDeleteItem(item.id)}
+              onEdit={() => handleEditItem(item.id)}
+              title={item.name}
+              date={item.createdAt}
+              description={item.description}
+            />
+          )}
+          keyExtractor={(item) => item.id}
+          ListEmptyComponent={
+            <LoadData
+              image="PRIMARY"
+              title="Desculpe!"
+              subtitle="Você ainda não possui dados para exibir aqui! Comece adicionando uma nova anotação clicando em Novo +."
+            />
+          }
+        />
       </Container>
 
       <Modal
@@ -69,7 +82,12 @@ export function Notes() {
         visible={showNotesModal}
         onRequestClose={closeModals}
       >
-        <NewNotes selectedItemId={selectedItemId} showButtonSave showButtonRemove closeBottomSheet={closeModals} />
+        <NewNotes
+          selectedItemId={selectedItemId}
+          showButtonSave
+          showButtonRemove
+          closeBottomSheet={closeModals}
+        />
       </Modal>
     </DefaultContainer>
   );
