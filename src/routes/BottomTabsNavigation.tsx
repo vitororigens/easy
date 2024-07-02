1fd0d3f630686568ea59509c498ee9db0ae90ef1
devjpedro <joaopedross.ti9@gmail.com>
2024-07-02 20:29:37 -0300

    integrando storage pro user

--- src/routes/BottomTabsNavigation.tsx
+++ src/routes/BottomTabsNavigation.tsx
@@ -1,107 +1,104 @@
-import { Entypo, FontAwesome } from '@expo/vector-icons';
-import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { Entypo, FontAwesome } from "@expo/vector-icons";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useTheme } from "styled-components/native";
-import LogoClipBoard from '../assets/Icones/icones_brokerx_cinza-18.svg';
-import LogoHome from '../assets/Icones/icones_brokerx_cinza-19.svg';
-import LogoCart from '../assets/Icones/icones_brokerx_cinza-25.svg';
-import { Loading } from '../components/Loading';
-import { Gears } from '../screens/Gears';
-import { Marketplace } from '../screens/Marketplace';
+import LogoClipBoard from "../assets/Icones/icones_brokerx_cinza-18.svg";
+import LogoHome from "../assets/Icones/icones_brokerx_cinza-19.svg";
+import LogoCart from "../assets/Icones/icones_brokerx_cinza-25.svg";
+import { Loading } from "../components/Loading";
+import { Gears } from "../screens/Gears";
+import { Marketplace } from "../screens/Marketplace";
 
-import { Platform } from 'react-native';
-import { Home } from '../screens/Home';
-import { ListTask } from '../screens/ListTask';
-import { Notes } from '../screens/Notes';
+import { Platform } from "react-native";
+import { Home } from "../screens/Home";
+import { ListTask } from "../screens/ListTask";
+import { Notes } from "../screens/Notes";
 
 const { Navigator, Screen } = createBottomTabNavigator();
-const tabBarHeight = Platform.OS === 'ios' ? 80 : 60;
+const tabBarHeight = Platform.OS === "ios" ? 80 : 60;
 
 export function BottomTabsNavigation() {
-    const { COLORS, FONTE_SIZE, FONT_FAMILY } = useTheme();
-    const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
+  const { COLORS, FONTE_SIZE, FONT_FAMILY } = useTheme();
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const subscriber = auth().onAuthStateChanged(setUser);
-        setTimeout(() => {
-            setIsLoading(false); 
-        }, 3000); 
-        return subscriber;
-    }, []);
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(setUser);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return subscriber;
+  }, []);
 
-    if (isLoading || user === null) {
-        return <Loading />;
-    }
-    return (
-        <Navigator
-            initialRouteName="Home"
-            screenOptions={{
-                headerShown: false,
-                tabBarStyle: {
-                    position: 'absolute',
-                    backgroundColor: COLORS.WHITE,
-                    height: tabBarHeight,
-                    borderTopWidth: 0,
-                    borderTopRightRadius: 30,
-                    borderTopLeftRadius: 30
-                },
-                tabBarLabelStyle: {
-                    display: 'none'
-                },
-                tabBarActiveTintColor: COLORS.TEAL_600,
-                
-            }}
-        >
+  if (isLoading || user === null) {
+    return <Loading />;
+  }
 
-            <Screen
-                options={{
-                    tabBarIcon: ({ color }) => (
-                        <LogoCart width={50} height={50} fill={color}/>
-                    )
-                }}
-                name="Mercado"
-                component={Marketplace}
-            />
-            <Screen
-                options={{
-                    tabBarIcon: ({ color }) => (
-                        <LogoClipBoard width={50} height={50} fill={color}/>
-                    )
-                }}
-                name="Tarefas"
-                component={ListTask}
-            />
-            <Screen
-                options={{
-                    tabBarIcon: ({ color }) => (
-                        <LogoHome width={50} height={50} fill={color}/>
-                    )
-
-                }}
-                name="Home"
-                component={Home}
-
-            />
-          <Screen
-                options={{
-                    tabBarIcon: ({ color }) => (
-                        <FontAwesome name="sticky-note" size={40} color={color} />
-                    )
-                }}
-                name="Notas"
-                component={Notes}
-            />
-            <Screen
-                options={{
-                    tabBarIcon: ({ color }) => (
-                        <Entypo name="menu" size={45} color={color} />
-                    )
-                }}
-                name="Config"
-                component={Gears}
-            />
-        </Navigator>
-    )
+  return (
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          backgroundColor: COLORS.WHITE,
+          height: tabBarHeight,
+          borderTopWidth: 0,
+          borderTopRightRadius: 30,
+          borderTopLeftRadius: 30,
+        },
+        tabBarLabelStyle: {
+          display: "none",
+        },
+        tabBarActiveTintColor: COLORS.TEAL_600,
+      }}
+    >
+      <Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <LogoCart width={50} height={50} fill={color} />
+          ),
+        }}
+        name="Mercado"
+        component={Marketplace}
+      />
+      <Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <LogoClipBoard width={50} height={50} fill={color} />
+          ),
+        }}
+        name="Tarefas"
+        component={ListTask}
+      />
+      <Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <LogoHome width={50} height={50} fill={color} />
+          ),
+        }}
+        name="Home"
+        component={Home}
+      />
+      <Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="sticky-note" size={40} color={color} />
+          ),
+        }}
+        name="Notas"
+        component={Notes}
+      />
+      <Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Entypo name="menu" size={45} color={color} />
+          ),
+        }}
+        name="Config"
+        component={Gears}
+      />
+    </Navigator>
+  );
 }
--- src/screens/Gears/index.tsx
+++ src/screens/Gears/index.tsx
@@ -1,19 +1,30 @@
-import { Entypo, FontAwesome, FontAwesome5 } from '@expo/vector-icons';
+import { Entypo, FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 import auth from "@react-native-firebase/auth";
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import { ScrollView, View } from "react-native";
+import { MMKV } from "react-native-mmkv";
 import { useTheme } from "styled-components/native";
 import { CustomModal } from "../../components/CustomModal";
 import { DefaultContainer } from "../../components/DefaultContainer";
 import { Loading } from "../../components/Loading";
 import { useUserAuth } from "../../hooks/useUserAuth";
-import { ButtonIcon, Content, ContentItems, Divider, Header, Icon, Items, Title } from "./styles";
+import {
+  ButtonIcon,
+  Content,
+  ContentItems,
+  Divider,
+  Header,
+  Icon,
+  Items,
+  Title,
+} from "./styles";
 
+const storage = new MMKV();
 
 export function Gears() {
-  const navigation = useNavigation()
-  const { COLORS } = useTheme()
+  const navigation = useNavigation();
+  const { COLORS } = useTheme();
   const user = useUserAuth();
   const [confirmLogoutVisible, setConfirmLogoutVisible] = useState(false);
   const [confirmDeleteVisible, setConfirmDeleteVisible] = useState(false);
@@ -25,7 +36,13 @@ export function Gears() {
   function handleLogout() {
     auth()
       .signOut()
-      .then(() => console.log('User signed out'));
+      .then(() => {
+        storage.delete("user");
+        console.log("User signed out");
+      })
+      .catch((error) => {
+        console.error("Error signing out: ", error);
+      });
   }
 
   function handleDeleteUserConfirmation() {
@@ -33,99 +50,104 @@ export function Gears() {
   }
 
   function handleDeleteUser() {
-
-    auth().currentUser?.delete()
+    auth().currentUser?.delete();
   }
 
   function handlePiggyBank() {
-    navigation.navigate('piggybank')
+    navigation.navigate("piggybank");
   }
-
 
   function handleGraphics() {
-    navigation.navigate('graphics')
+    navigation.navigate("graphics");
   }
   function handlePefil() {
-    navigation.navigate('perfil')
+    navigation.navigate("perfil");
   }
 
-
   return (
-    <DefaultContainer title='Configurações'>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Content>
-            {user ?
-              <View>
-                <Header>
-                  <Divider />
-                </Header>
-                <ContentItems>
-
-                  <ButtonIcon onPress={handleGraphics}>
-                    <Items>
-                      <Title>
-                        Gráficos
-                      </Title>
-                     <Icon>
-                     <FontAwesome name="line-chart" size={30} color={COLORS.PURPLE_800} />
-                     </Icon>
-
-                    </Items>
-                  </ButtonIcon>
-                  <ButtonIcon onPress={handlePiggyBank}>
-                    <Items>
-                      <Title>
-                        Cofrinho
-                      </Title>
-                      <Icon>
-                      <FontAwesome5 name="piggy-bank" size={30} color={COLORS.PURPLE_800} />
-                      </Icon>
-
-                    </Items>
-                  </ButtonIcon>
-                  <ButtonIcon onPress={handlePefil}>
-                    <Items>
-                      <Title>
-                        Perfil
-                      </Title>
-                      <Icon>
-                        <FontAwesome name="user" size={30} color={COLORS.PURPLE_800} />
-                      </Icon>
-                    </Items>
-                  </ButtonIcon>
-                  <ButtonIcon onPress={handleDeleteUserConfirmation}>
-                    <Items>
-                      <Title>
-                        Deletar Conta
-                      </Title>
-                      <Icon>
-                        <FontAwesome name="trash" size={30} color={COLORS.PURPLE_800} />
-                      </Icon>
-
-                    </Items>
-                  </ButtonIcon>
-                  <ButtonIcon onPress={handleLogoutConfirmation}>
-                    <Items>
-                      <Title>
-                        Sair
-                      </Title>
-                      <Icon>
-                        <Entypo name="log-out" size={30} color={COLORS.PURPLE_800} />
-                      </Icon>
-
-                    </Items>
-                  </ButtonIcon>
-                </ContentItems>
-              </View>
-              : <Loading />
-            }
-          </Content>
-        </ScrollView>
+    <DefaultContainer title="Configurações">
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Content>
+          {user ? (
+            <View>
+              <Header>
+                <Divider />
+              </Header>
+              <ContentItems>
+                <ButtonIcon onPress={handleGraphics}>
+                  <Items>
+                    <Title>Gráficos</Title>
+                    <Icon>
+                      <FontAwesome
+                        name="line-chart"
+                        size={30}
+                        color={COLORS.PURPLE_800}
+                      />
+                    </Icon>
+                  </Items>
+                </ButtonIcon>
+                <ButtonIcon onPress={handlePiggyBank}>
+                  <Items>
+                    <Title>Cofrinho</Title>
+                    <Icon>
+                      <FontAwesome5
+                        name="piggy-bank"
+                        size={30}
+                        color={COLORS.PURPLE_800}
+                      />
+                    </Icon>
+                  </Items>
+                </ButtonIcon>
+                <ButtonIcon onPress={handlePefil}>
+                  <Items>
+                    <Title>Perfil</Title>
+                    <Icon>
+                      <FontAwesome
+                        name="user"
+                        size={30}
+                        color={COLORS.PURPLE_800}
+                      />
+                    </Icon>
+                  </Items>
+                </ButtonIcon>
+                <ButtonIcon onPress={handleDeleteUserConfirmation}>
+                  <Items>
+                    <Title>Deletar Conta</Title>
+                    <Icon>
+                      <FontAwesome
+                        name="trash"
+                        size={30}
+                        color={COLORS.PURPLE_800}
+                      />
+                    </Icon>
+                  </Items>
+                </ButtonIcon>
+                <ButtonIcon onPress={handleLogoutConfirmation}>
+                  <Items>
+                    <Title>Sair</Title>
+                    <Icon>
+                      <Entypo
+                        name="log-out"
+                        size={30}
+                        color={COLORS.PURPLE_800}
+                      />
+                    </Icon>
+                  </Items>
+                </ButtonIcon>
+              </ContentItems>
+            </View>
+          ) : (
+            <Loading />
+          )}
+        </Content>
+      </ScrollView>
       <CustomModal
         animationType="slide"
         transparent={true}
         onCancel={() => setConfirmLogoutVisible(false)}
-        onClose={() => { setConfirmLogoutVisible(false); }}
+        onClose={() => {
+          setConfirmLogoutVisible(false);
+        }}
         onConfirme={() => {
           setConfirmLogoutVisible(false);
           handleLogout();
@@ -137,15 +159,16 @@ export function Gears() {
         animationType="slide"
         transparent={true}
         onCancel={() => setConfirmDeleteVisible(false)}
-        onClose={() => { setConfirmDeleteVisible(false); }}
+        onClose={() => {
+          setConfirmDeleteVisible(false);
+        }}
         onConfirme={() => {
           setConfirmDeleteVisible(false);
-          handleDeleteUser()
+          handleDeleteUser();
         }}
         title="Deseja realmente excluir essa conta?"
         visible={confirmDeleteVisible}
       />
-
     </DefaultContainer>
   );
 }
